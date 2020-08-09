b03e2cf839b7b068ff6a97f3ae9d937898f58988
Joao Veloso <joao.goncalo.veloso@gmail.com>
2020-08-09 17:52:38 +0100

    database and models

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,3 +1,21 @@
+from datetime import time
+
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    floor_number = models.IntegerField(default=0)
+    room_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} on floor {self.floor_number} on room {self.room_number}"
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)#foreignkey adds from another table (on delete is mandatory)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 
 
 def welcome(request):
-    return HttpResponse("Welcome to the meeting Planner!")
+    return render(request, "website/welcome.html",
+                  {"message": "This data was sent from view to the template"})
 
 def date(request):
     return HttpResponse("This page was served at" + str(datetime.now()))
