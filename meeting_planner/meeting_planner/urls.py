caf218775919f0f15d49ab0d1af4c196ad57e0ff
Joao Veloso <joao.goncalo.veloso@gmail.com>
2020-08-11 12:49:29 +0100

    static and templates

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from website.views import welcome, date, about_me
-from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name='home'),
     path('date', date),
     path('about_me', about_me),
-    path('meetings/<int:id>', detail, name='detail')
+    path('meetings/', include('meetings.urls')),
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
-from meetings.models import Meeting
+from meetings.models import Meeting, Room
 
 
 def detail(request, id):
     #    meeting = Meeting.objects.get(pk=id) pk->primary key-> the row with this unique id
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
+
+
+def rooms(request):
+    return render(request, "meetings/rooms.html",
+                  {"rooms": Room.objects.all()})
+
+def new(request):
+    return render(request, "meetings/new.html")
